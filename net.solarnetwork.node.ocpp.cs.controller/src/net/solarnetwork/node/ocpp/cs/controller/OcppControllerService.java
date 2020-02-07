74a1b6a056aa763fc5629f6304fe19c560672d55
Matt Magoffin <matt@solarnetwork.net>
2020-02-07 17:16:50 +1300

    Actually persist ChargePoint entities and save changes during BootNotification processing.

--- net.solarnetwork.node.ocpp.cs.controller/src/net/solarnetwork/node/ocpp/cs/controller/OcppControllerService.java
+++ net.solarnetwork.node.ocpp.cs.controller/src/net/solarnetwork/node/ocpp/cs/controller/OcppControllerService.java
@@ -22,6 +22,7 @@
 
 package net.solarnetwork.node.ocpp.cs.controller;
 
+import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Propagation;
@@ -29,6 +30,7 @@ import org.springframework.transaction.annotation.Transactional;
 import net.solarnetwork.node.ocpp.dao.ChargePointDao;
 import net.solarnetwork.node.ocpp.domain.ChargePoint;
 import net.solarnetwork.node.ocpp.domain.ChargePointInfo;
+import net.solarnetwork.node.ocpp.domain.RegistrationStatus;
 import net.solarnetwork.node.ocpp.service.cs.ChargePointManager;
 import net.solarnetwork.node.support.BaseIdentifiable;
 
@@ -40,7 +42,11 @@ import net.solarnetwork.node.support.BaseIdentifiable;
  */
 public class OcppControllerService extends BaseIdentifiable implements ChargePointManager {
 
+	/** The default {@code initialRegistrationStatus} value. */
+	public static final RegistrationStatus DEFAULT_INITIAL_REGISTRATION_STATUS = RegistrationStatus.Pending;
+
 	private final ChargePointDao chargePointDao;
+	private RegistrationStatus initialRegistrationStatus;
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -52,13 +58,75 @@ public class OcppControllerService extends BaseIdentifiable implements ChargePoi
 	public OcppControllerService(ChargePointDao chargePointDao) {
 		super();
 		this.chargePointDao = chargePointDao;
+		this.initialRegistrationStatus = DEFAULT_INITIAL_REGISTRATION_STATUS;
 	}
 
 	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	@Override
-	public ChargePoint registerChargePoint(ChargePointInfo chargePoint) {
-		log.info("Charge Point registration received: {}", chargePoint);
-		return null;
+	public ChargePoint registerChargePoint(ChargePointInfo info) {
+		log.info("Charge Point registration received: {}", info);
+
+		if ( info == null || info.getId() == null ) {
+			throw new IllegalArgumentException("The ChargePoint ID must be provided.");
+		}
+
+		ChargePoint cp = chargePointDao.get(info.getId());
+		if ( cp == null ) {
+			cp = registerNewChargePoint(info);
+		} else if ( cp.isEnabled() ) {
+			cp = updateChargePointInfo(cp, info);
+		}
+
+		return cp;
+	}
+
+	private ChargePoint registerNewChargePoint(ChargePointInfo info) {
+		log.info("Registering new ChargePoint {}", info);
+		ChargePoint cp = new ChargePoint(info.getId(), Instant.now());
+		cp.setEnabled(true);
+		cp.setRegistrationStatus(getInitialRegistrationStatus());
+		cp.setInfo(info);
+		chargePointDao.save(cp);
+		return cp;
+	}
+
+	private ChargePoint updateChargePointInfo(ChargePoint cp, ChargePointInfo info) {
+		assert cp != null && cp.getInfo() != null;
+		if ( cp.getInfo().isSameAs(info) ) {
+			log.info("ChargePoint registration info is unchanged: {}", info);
+		} else {
+			log.info("Updating ChargePoint registration info {} -> {}", cp.getInfo(), info);
+			cp.setInfo(info);
+			chargePointDao.save(cp);
+		}
+		return cp;
+	}
+
+	/**
+	 * Get the initial {@link RegistrationStatus} to use for newly registered
+	 * charge points.
+	 * 
+	 * @return the status, never {@literal null}
+	 */
+	public RegistrationStatus getInitialRegistrationStatus() {
+		return initialRegistrationStatus;
+	}
+
+	/**
+	 * Set the initial {@link RegistrationStatus} to use for newly registered
+	 * charge points.
+	 * 
+	 * @param initialRegistrationStatus
+	 *        the status to set
+	 * @throws IllegalArgumentException
+	 *         if {@code initialRegistrationStatus} is {@literal null}
+	 */
+	public void setInitialRegistrationStatus(RegistrationStatus initialRegistrationStatus) {
+		if ( initialRegistrationStatus == null ) {
+			throw new IllegalArgumentException(
+					"The initialRegistrationStatus parameter must not be null.");
+		}
+		this.initialRegistrationStatus = initialRegistrationStatus;
 	}
 
 }
